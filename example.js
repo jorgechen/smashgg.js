22d71957ba02723ef71b658c8ad39841f9cf30dd
george <sylvanknave@gmail.com>
2019-08-13 00:54:36 -0500

    example.js

---
+++ example.js
@@ -0,0 +1,74 @@
+const _ = require('lodash')
+const smashgg = require('./')
+smashgg.initialize('3454a7a2f7457a4565c6546775745e40')
+require('dotenv').config()
+require('colors')
+
+// smashgg.setLogLevel('queries')
+const { Event, League, Tournament, VideoGame } = smashgg
+
+;(async () => {
+  const filter = {
+    past: true,
+    isFeatured: true,
+    videogameIds: [3200],
+  }
+  const videogames = await VideoGame.getTournamentsRaw(filter)
+  videogames.forEach(({ name, startAt, endAt }) => {
+    console.log(_.padStart(name, 70), new Date(startAt * 1000), new Date(endAt * 1000))
+  })
+  console.log(`${videogames.length} games found`)
+  console.log(videogames[0])
+
+  return true
+
+  const leagueSlug = 'the-2019-mortal-kombat-pro-kompetition'
+  const league = await League.get(leagueSlug)
+  console.info(league.getName())
+
+  // NOTE: this is currently lacking data like player's points
+  // const leagueStandings = await league.getStandingsRaw()
+  // console.log(leagueStandings)
+
+  const events = await league.getEvents()
+  const event = events[0]
+  console.info(event)
+  const tourney = await event.getTournamentRaw()
+  console.log(tourney)
+
+  let sets = await event.getSetsRaw()
+  console.info('Got %s sets played', sets.length)
+  console.info('Sample sets:')
+  console.info(JSON.stringify(sets.slice(sets.length - 4), null, 2))
+
+  // const standings = await event.getStandingsRaw()
+  // console.info(`Found ${standings.length} standings`)
+  // // console.info(JSON.stringify(standings.slice(0, 2), null, 2))
+  //
+  // // Each standing has a player
+  // const newStandings = []
+  // const newPlayers = []
+  // standings.forEach(standing => {
+  //   const newStanding = {
+  //     sggId: standing.id,
+  //     rank: standing.placement,
+  //   }
+  //   const { player } = standing.entrant.participants[0]
+  //   const newPlayer = {
+  //     sggId: player.id,
+  //     team: player.prefix,
+  //     handle: player.gamerTag,
+  //     twitter: player.twitterHandle,
+  //     twitch: player.twitchStream,
+  //     youtube: player.youtube,
+  //     country: player.country,
+  //     state: player.state,
+  //     portrait: player.images[0].url, // TODO get best one
+  //   }
+  //   console.log(newStanding)
+  //   console.log(newPlayer)
+  //   newStandings.push(newStanding)
+  //   newPlayers.push(newPlayer)
+  // })
+  return true // exit async
+})()
